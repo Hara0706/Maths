0335523a06f4a416dd87acdb49dafae3d26392d6
Harapriya Mohapatra <73417004+Hara0706@users.noreply.github.com>
2021-08-06 07:20:36 +0530

    Create bigint.cpp

---
+++ bigint.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+
+int main() {
+    
+    ll x; cin>>x;
+    ll ans = 4*pow(x, 3) + 5*pow(x, 2) - 6*x + 14;
+    
+    cout<<ans<<endl;
+    
+	return 0;
+}
+
